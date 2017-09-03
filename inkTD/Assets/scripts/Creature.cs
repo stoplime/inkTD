f77b0b9fcdd8a79f2a3fc7ddcdc3e1d67531cff6
stoplime <slanimero@gmail.com>
2017-09-03 19:44:33 -0400

    center the arrows

--- inkTD/Assets/scripts/Creature.cs
+++ inkTD/Assets/scripts/Creature.cs
@@ -2,46 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Creature : MonoBehaviour {
-
-	[Tooltip("The base damage value excluding all modifiers. Example 10")]
-    public float damage;
-
-    [Tooltip("The number of projectiles fired per minute. Example: 30")]
-    public float speed;
+public class Creature : InkObject
+{
     
-    [Tooltip("The distance in all directions around the tower in world coordinates that can be fired upon. Example: 5")]
-    public float range;
 
-    [Tooltip("The cost to purchase this tower. Example 50")]
-    public int price;
-
-    /// <summary>
-    /// This variable is no longer used after the tower has been created. 
-    /// </summary>
-    [Tooltip("The position along the playing grid's x axis.")]
-    public int initialGridPositionX = 0;
-
-    /// <summary>
-    /// This variable is no longer used after the tower has been created.
-    /// </summary>
-    [Tooltip("The position along the playing grid's y axis.")]
-    public int initialGridPositionY = 0;
-
-    [Tooltip("The total lifetime in milliseconds the projectile will live.")]
-    public float projectileLife = 1000;
-
-    [Tooltip("The object that is shot when this tower attacks.")]
-    public GameObject projectileObject;
-
-    [Tooltip("The height at which projectiles are spawned.")]
-    public float projectileSpawnHeight;
-
-    [Tooltip("The sound effect that plays whenever a projectile is fired from this tower.")]
-    public AudioClip shootSoundEffect;
-
-    [Tooltip("The ID of the player which owns this tower.")]
-    public int ownerID = 0;
 
 	// Use this for initialization
 	void Start () {
--- inkTD/Assets/scripts/InkObject.cs
+++ inkTD/Assets/scripts/InkObject.cs
@@ -3,55 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using helper;
 
-public class InkObject : MonoBehaviour {
+public abstract class InkObject : MonoBehaviour
+{
 
+    /// <summary>
+    /// Creatures have increased damage done to th tower-castle 
+    /// Towers have increased projectile damage
+    /// </summary>
 	[Tooltip("The base damage value excluding all modifiers. Example 10")]
     public float damage;
 
+    /// <summary>
+    /// Creatues have faster speed traversing the map
+    /// Towers have faster reloading of projectiles
+    /// </summary>
     [Tooltip("The number of projectiles fired per minute. Example: 30")]
     public float speed;
-    
-    [Tooltip("The distance in all directions around the tower in world coordinates that can be fired upon. Example: 5")]
-    public float range;
 
+    /// <summary>
+    /// Ink cost for both towers and creatures
+    /// </summary>
     [Tooltip("The cost to purchase this tower. Example 50")]
     public int price;
 
     /// <summary>
-    /// This variable is no longer used after the tower has been created. 
+    /// This variable is no longer used after the tower/creature has been created. 
     /// </summary>
     [Tooltip("The position along the playing grid's x axis.")]
     public int initialGridPositionX = 0;
 
     /// <summary>
-    /// This variable is no longer used after the tower has been created.
+    /// This variable is no longer used after the tower/creature has been created.
     /// </summary>
     [Tooltip("The position along the playing grid's y axis.")]
     public int initialGridPositionY = 0;
 
-    [Tooltip("The total lifetime in milliseconds the projectile will live.")]
-    public float projectileLife = 1000;
-
-    [Tooltip("The object that is shot when this tower attacks.")]
-    public GameObject projectileObject;
-
-    [Tooltip("The height at which projectiles are spawned.")]
-    public float projectileSpawnHeight;
-
-    [Tooltip("The sound effect that plays whenever a projectile is fired from this tower.")]
-    public AudioClip shootSoundEffect;
-
-    [Tooltip("The ID of the player which owns this tower.")]
+    /// <summary>
+    /// ID for the grid parent.
+    /// For towers, the id represent which grid it's on.
+    /// Creatures, the ID represent which grid it's not on.
+    /// Ex. creature id is the player, then the creature is spawned on all grids that's not the players.
+    /// </summary>
+    [Tooltip("The ID of the player which owns this tower/creature.")]
     public int ownerID = 0;
 
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
 }
