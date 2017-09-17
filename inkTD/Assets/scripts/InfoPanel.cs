0d27e309ab759def6ef73b1e2a51f18b3c94fb25
Jackson <jackdavis7118@gmail.com>
2017-09-16 20:45:45 -0400

    Added Camera Bezier
    All scene changes are in "ui test.unity"

---
+++ inkTD/Assets/scripts/InfoPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel : MonoBehaviour
+{
+
+    public Texture image;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    public static void RecieveTowerInfo(string towerName, string description, int cost, int damage, int rateOfFire)
+    {
+
+    }
+
+    public static void RecieveCreatureInfo(string creatureName, string description, int cost, int damage)
+    {
+
+    }
+
+    void ClearMenu()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
