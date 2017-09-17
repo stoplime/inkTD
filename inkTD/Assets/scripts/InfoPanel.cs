10ee973843f8202ae1e8d925763f71b06b04b78b
Jackson <jackdavis7118@gmail.com>
2017-09-16 23:53:55 -0400

    UI commit

--- inkTD/Assets/scripts/InfoPanel.cs
+++ inkTD/Assets/scripts/InfoPanel.cs
@@ -1,36 +1,117 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoPanel : MonoBehaviour
 {
 
-    public Texture image;
+    public Image image;
+    public Text nameText;
+    public Text costText;
+    public Text rateOfFireText;
+    public Text damageText;
+    public Text descriptionText;
+    public Text healthText;
+    public Button buyButton;
 
-	// Use this for initialization
-	void Start ()
+    public string objName, description;
+    public float cost, health, rateOfFire, damage;
+
+
+    // Use this for initialization
+    void Start ()
     {
-		
+        ClearMenu();
 	}
 
-    public static void RecieveTowerInfo(string towerName, string description, int cost, int damage, int rateOfFire)
+    /// <summary>
+    /// A method used to recieve the tower information from a button clicked in the buy menu
+    /// </summary>
+    /// <param name="towerName"></param>
+    /// <param name="description"></param>
+    /// <param name="cost"></param>
+    /// <param name="damage"></param>
+    /// <param name="rateOfFire"></param>
+    /// <param name="image"></param>
+    public void RecieveTowerInfo(string towerName, string description, float cost, float damage, float rateOfFire, Sprite image)
+    {
+        objName = towerName;
+        this.description = description;
+        this.cost = cost;
+        this.damage = damage;
+        this.rateOfFire = rateOfFire;
+        this.image.overrideSprite = image;
+    }
+
+    /// <summary>
+    /// A method used to recieve the creature information from a button clicked in the buy menu
+    /// </summary>
+    /// <param name="creatureName"></param>
+    /// <param name="description"></param>
+    /// <param name="health"></param>
+    /// <param name="cost"></param>
+    /// <param name="damage"></param>
+    /// <param name="image"></param>
+    public void RecieveCreatureInfo(string creatureName, string description, float health, float cost, float damage, Sprite image)
+    {
+        objName = creatureName;
+        this.description = description;
+        this.health = health;
+        this.cost = cost;
+        this.damage = damage;
+        this.image.overrideSprite = image;
+    }
+
+    /// <summary>
+    /// A method used to recieve the modifier information from a button clicked in the buy menu
+    /// </summary>
+    /// <param name="modName"></param>
+    /// <param name="description"></param>
+    /// <param name="cost"></param>
+    public static void RecieveModifierInfo(string modName, string description, float cost)
     {
 
     }
 
-    public static void RecieveCreatureInfo(string creatureName, string description, int cost, int damage)
-    {
-
-    }
-
+    /// <summary>
+    /// A method used for clearing the information in the InfoPanel
+    /// </summary>
     void ClearMenu()
     {
+        nameText.text = "";
+        descriptionText.text = "";
+        costText.text = "";
+        damageText.text = "";
+        
+        if (rateOfFireText != null)
+        {
+            rateOfFireText.text = "";
+        }
 
+        if (healthText != null)
+        {
+            healthText.text = "";
+        }
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        nameText.text = objName;
+        descriptionText.text = description;
+        costText.text = "Cost: " + Convert.ToString(cost);
+        damageText.text = "Damage: " + Convert.ToString(damage);
+
+        if (rateOfFireText != null)
+        {
+            rateOfFireText.text = "Rate of Fire: " + Convert.ToString(rateOfFire);
+        }
+
+        if (healthText != null)
+        {
+            healthText.text = "Health: " + Convert.ToString(health);
+        }
+    }
 }
--- inkTD/Assets/scripts/MenuButton.cs
+++ inkTD/Assets/scripts/MenuButton.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
-    public string towerName, description;
-    public int cost, damage, rateOfFire;
+    public string objName, description;
+    public float health, cost, damage, rateOfFire;
+    public Sprite image;
 
+    public enum ButtonType { Tower, Creature, Modifier };
+
+    public ButtonType type;
+
+    public InfoPanel infoPanel;
 
 	// Use this for initialization
 	void Start ()
@@ -14,9 +21,30 @@ public class MenuButton : MonoBehaviour
 		
 	}
 
+    /// <summary>
+    /// A method that sends certain pieces of information to the InfoPanel, based on which BuyPanel the button is in
+    /// </summary>
     public void OnClick()
     {
-        
+        switch(type)
+        {
+            case ButtonType.Tower:
+                infoPanel.RecieveTowerInfo(objName, description, cost, damage, rateOfFire, image);
+                Debug.Log("Ok");
+                break;
+
+            case ButtonType.Creature:
+                infoPanel.RecieveCreatureInfo(objName, description, health, cost, damage, image);
+                break;
+
+            case ButtonType.Modifier:
+                //InfoPanel.RecieveModifierInfo();
+                break;
+
+            default:
+                Debug.Log("No type assigned to this button");
+                break;
+        }
     }
 
     // Update is called once per frame
