e4c6a4ae2b7d297b972201f55a537e79d8341122
stoplime <slanimero@gmail.com>
2017-10-03 11:18:45 -0400

    started AI

--- inkTD/Assets/scripts/EnemyAI.cs
+++ inkTD/Assets/scripts/EnemyAI.cs
@@ -2,6 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
+AI PLANS
+
+Tower placement
+- go through the optimal path backwards and look and the adjacent nodes on the path
+  to find an available placement.
+- use a weighted random selection between adjacent nodes to place
+
+Creature spawning
+- hard coded strategies that will use combinations of creatures
+- spawn based off a random weighted list of actions, where an action is a creature or combination
+
+ */
+
 public class EnemyAI : MonoBehaviour {
 
 	public int ID;
