25175e6139347bf5c3b2fc029889cf2573865ba6
stoplime <slanimero@gmail.com>
2017-11-06 10:08:39 -0500

    start setup of obstacles

---
+++ inkTD/Assets/scripts/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+	public float Price;
+
+	public Vector2 ObstacleSize;
+
+	public GameObject PlaceHolderObject;
+
+	private GameObject[,] PlaceHolders;
+
+	// Use this for initialization
+	void Start () {
+		PlaceHolders = new GameObject[ObstacleSize.x, ObstacleSize.y];
+		foreach (GameObject i in PlaceHolders)
+		{
+			i = PlaceHolderObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
